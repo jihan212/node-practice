9a9ec6889111264834348ca225e9775a81aade54
snapper <zihanjashim2121@gmail.comm>
2021-03-26 01:39:24 +0600

    node practice

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(cors());
+app.use(bodyParser.json());
+// ------------- get --------------
 app.get ( '/', (req, res) => {
-    res.send ('Yeeeyyy I know how to see node');
+    res.send ('Yeeeyyy I know node a bit');
+})
+
+app.get('/users/:id', (req, res) => {
+    const users = ['Aisha','Rishab','Sahil','Kanishk','Koustob'];
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id , name});
+})
+
+// ------------ post --------------
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    user.id = 15;
+    res.send(user);
 })
 
 app.listen ( 2121, ()=> console.log('Listening to port 2121'));
